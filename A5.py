830b84bc763e9ac2e50a04a312e3fc48cc9428ec
cavdb39 <145156551+cavdb39@users.noreply.github.com>
2024-11-24 21:29:48 +0100

    Clean up code

--- A5.py
+++ A5.py
@@ -1,24 +1,3 @@
-def shuffle(mode: str) -> list:
-    if mode not in "brl":
-        print(f"skipping invalid mode '{mode}'.")
-        return
-    global cards
-    if mode == "b":
-        cards.append(cards.pop(0))
-        return
-    shuffled_cards = []
-    left_part, right_part = cards[:N], cards[N:]
-    if mode == "r":
-        for _ in range(N):
-            shuffled_cards.append(left_part.pop(0))
-            shuffled_cards.append(right_part.pop(0))
-    elif mode == "l":
-        for _ in range(N):
-            shuffled_cards.append(right_part.pop(0))
-            shuffled_cards.append(left_part.pop(0))
-    cards = shuffled_cards
-
-
 # Handle input
 N = int(input())
 operation = input()
@@ -26,22 +5,20 @@ operation = input()
 # Construct list
 cards = [i for i in range(1, 2 * N + 1)]
 
+# Initiate variables
 split_operation = []
+shuffled_cards = []
 temp = ""
-
 processed_operation = ""
 
 # Splitting the input
 for char in operation:
-    if char.isdigit():
-        if temp and not (temp[-1].isdigit() or temp[-1] == "("):
-            split_operation.append(temp)
-            temp = ""
+    if char.isdigit() and (temp and not temp[-1].isdigit() and temp[-1] != "("):
+        split_operation.append(temp)
+        temp = ""
     temp += char
 
 split_operation.append(temp)
-
-print(split_operation)
 
 # Process each task
 for task in split_operation:
@@ -63,32 +40,48 @@ for task in split_operation:
         start_index = text.find("(")
         end_index = text.find(")")
 
-        a = text[start_index + 1 : end_index]
-        b = text[end_index + 1 :]
-
-        print(a)
-
-        for char in a:
-            if not char.isdigit():
-                index_2 = a.index(char)
-                break
-
-        remaining = a[index_2 + 1 :] if index_2 + 1 < len(a) else ""
-        if index_2:
-            a = int(a[:index_2]) * a[index_2] + remaining
+        if start_index + 1 == end_index:
+            remaining = text[1:] if len(text) > 1 else ""
+            processed_operation += num * text[0] + remaining
         else:
-            a[index_2]
+            a = text[start_index + 1 : end_index]
+            b = text[end_index + 1 :]
 
-        processed_operation += num * a + b
+            for char in a:
+                if not char.isdigit():
+                    index_2 = a.index(char)
+                    break
+
+            if index_2:
+                remaining = a[index_2 + 1 :] if index_2 + 1 < len(a) else ""
+                a = int(a[:index_2]) * a[index_2] + remaining
+            else:
+                a[index_2]
+
+            processed_operation += num * a + b
     else:
-        print(num, text)
         remaining = text[1:] if len(text) > 1 else ""
-        print(remaining)
         processed_operation += num * text[0] + remaining
 
-print(processed_operation)
-
 for char in processed_operation:
-    shuffle(char)
+    if char not in {"b", "r", "l"}:
+        print("skipping invalid mode:", char)
+        continue
+
+    if char == "b":
+        cards.append(cards.pop(0))
+        continue
+    else:
+        left_part, right_part = cards[:N], cards[N:]
+        if char == "r":
+            shuffled_cards.extend(
+                [card for pair in zip(left_part, right_part) for card in pair]
+            )
+        elif char == "l":
+            shuffled_cards.extend(
+                [card for pair in zip(right_part, left_part) for card in pair]
+            )
+        cards = shuffled_cards
+        shuffled_cards = []
 
 print(cards)
